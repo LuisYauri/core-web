bfbcda6e6a07babc8008b269373cad5cdf029b5b
Luis Yauri <luis.yauri1@unmsm.edu.pe>
2019-07-24 08:01:57 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import {AdministratorComponent} from './modules/administrator/administrator.component';
+import { LoginComponent } from './modules/login/login.component';
+import { SidebarComponent } from './components/header/sidebar/sidebar.component';
+import { NavbarComponent } from './components/header/navbar/navbar.component';
+import { EmployeesComponent } from './modules/administrator/employees/employees.component';
+import { ToolsComponent } from './modules/administrator/tools/tools.component';
+import { EntertainmentsComponent } from './modules/administrator/entertainments/entertainments.component';
+import { EventsComponent } from './modules/administrator/events/events.component';
+import { LaborComponent } from './modules/administrator/labor/labor.component';
+import { SchedulesComponent } from './modules/administrator/schedules/schedules.component';
+import { SuggestionsComponent } from './modules/administrator/suggestions/suggestions.component';
+import { ReportsComponent } from './modules/administrator/reports/reports.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    AdministratorComponent,
+    LoginComponent,
+    SidebarComponent,
+    NavbarComponent,
+    EmployeesComponent,
+    ToolsComponent,
+    EntertainmentsComponent,
+    EventsComponent,
+    LaborComponent,
+    SchedulesComponent,
+    SuggestionsComponent,
+    ReportsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/modules/administrator/entertainments/entertainments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-entertainments',
+  templateUrl: './entertainments.component.html',
+  styleUrls: ['./entertainments.component.scss']
+})
+export class EntertainmentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
